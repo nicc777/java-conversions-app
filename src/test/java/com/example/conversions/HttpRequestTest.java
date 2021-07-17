5d8548ac5769e10af91596d316f7b4a303531c93
Nico Coetzee <nicc777@gmail.com>
2021-07-17 15:31:56 +0200

    test: added basic controller tests

---
+++ src/test/java/com/example/conversions/HttpRequestTest.java
@@ -0,0 +1,30 @@
+/**
+ * Refer to https://spring.io/guides/gs/testing-web/ for setting up controller tests
+ */
+
+package com.example.conversions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HttpRequestTest {
+    
+    @Autowired
+	private MockMvc mockMvc;
+    
+    @Test
+    void convertCtoFTest() throws Exception {
+        mockMvc.perform(get("/v1/convert/c-to-f/15.0")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("59.0")));
+    }
+
+}
