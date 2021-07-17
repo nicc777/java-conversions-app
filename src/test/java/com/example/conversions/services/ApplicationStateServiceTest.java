0d587b73d37babdc7487fabfb4f8671a46105005
Nico Coetzee <nicc777@gmail.com>
2021-07-17 15:10:47 +0200

    test: updated unit tests

--- src/test/java/com/example/conversions/services/ApplicationStateServiceTest.java
+++ src/test/java/com/example/conversions/services/ApplicationStateServiceTest.java
@@ -3,10 +3,14 @@ package com.example.conversions.services;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.example.conversions.ConversionsDemoApplication;
+
 @SpringBootTest
+@ContextConfiguration(classes = ConversionsDemoApplication.class)
 public class ApplicationStateServiceTest {
     
     @Autowired 
--- src/test/java/com/example/conversions/utils/OsFunctionsTest.java
+++ src/test/java/com/example/conversions/utils/OsFunctionsTest.java
@@ -3,10 +3,14 @@ package com.example.conversions.utils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import com.example.conversions.ConversionsDemoApplication;
+
 @SpringBootTest
+@ContextConfiguration(classes = ConversionsDemoApplication.class)
 public class OsFunctionsTest {
     
     @Autowired
